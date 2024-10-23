85ada31edf1abfaaca2e64f0e4ed4809f8dc0133
DrDogg92 <DrDogg92@gmail.com>
2024-10-23 11:43:04 -0400

    feat: clean up known-tokens

--- libs/examples/src/known-tokens.ts
+++ libs/examples/src/known-tokens.ts
@@ -1,26 +1,5 @@
 import { sdk } from './setup';
 
-const padConfig = {
-  symbol: 12,
-  name: 36,
-  decimals: 4,
-  address: 44,
-  tokenType: 0
-};
-
-function logWithPadEnd(value: string, key: keyof typeof padConfig) {
-  return value.padEnd(padConfig[key], ' ');
-}
-
-function print(symbol: string, name: string, decimals: string, address: string, tokenType: string) {
-  console.log(
-    `${symbol.padEnd(padConfig.symbol, ' ')} ${name.padEnd(padConfig.name, ' ')} ${decimals.padEnd(
-      padConfig.decimals,
-      ' '
-    )} ${address.toLowerCase().padEnd(padConfig.address, ' ')} ${tokenType}`
-  );
-}
-
 sdk.tokens.getMap().forEach((value, key) => {
   const address = value.address.toString().toLowerCase();
   const decimals = value.decimals.toString();
