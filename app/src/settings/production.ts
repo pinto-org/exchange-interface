a6105321a595862f7c87b85b54b4ef6546c84a48
Sunsire <186860501+Sunsire@users.noreply.github.com>
2024-11-19 10:07:20 -0600

    fix prod config

--- app/src/settings/production.ts
+++ app/src/settings/production.ts
@@ -2,6 +2,6 @@ import { EnvDexSettings } from '.';
 
 export const ProdSettings: EnvDexSettings = {
   PRODUCTION: true,
-  WELLS_ORIGIN_BLOCK: 17977922,
-  LOAD_HISTORY_FROM_GRAPH: true
+  WELLS_ORIGIN_BLOCK: parseInt(import.meta.env.VITE_WELLS_ORIGIN_BLOCK) || 22622227,
+  LOAD_HISTORY_FROM_GRAPH: !!parseInt(import.meta.env.VITE_LOAD_HISTORY_FROM_GRAPH) || true
 };
