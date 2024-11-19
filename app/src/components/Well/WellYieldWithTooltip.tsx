a75c35c0c6a2d0d988d0003194ef0778a5b751ef
DrDogg92 <DrDogg92@gmail.com>
2024-11-19 10:47:01 -0700

    feat: change url

--- app/src/components/Well/WellYieldWithTooltip.tsx
+++ app/src/components/Well/WellYieldWithTooltip.tsx
@@ -67,7 +67,7 @@ export const WellYieldWithTooltip: React.FC<Props> = ({ tooltipProps, well, retu
               <div>
                 The Variable PINTO APY (vAPY) uses historical data of Beans earned by{' '}
                 <a
-                  href='https://docs.bean.money/almanac/guides/silo/understand-silo-vapy'
+                  href='https://docs.pintobean.money/almanac/guides/silo/understand-silo-vapy'
                   target='_blank'
                   rel='noopener noreferrer'
                   className='underlined'
