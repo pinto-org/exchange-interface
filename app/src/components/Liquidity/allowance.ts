419f18b0f94ce233de7ae5d1083711022f085234
DrDogg92 <DrDogg92@gmail.com>
2024-11-19 10:42:27 -0700

    feat: fix approve

--- app/src/components/Liquidity/allowance.ts
+++ app/src/components/Liquidity/allowance.ts
@@ -29,10 +29,11 @@ const ensureAllowance = async (
       error: 'Approval failed',
       success: 'Spending limit approved'
     });
+
     try {
       const approveTXN = await token
         .getContract()
-        .approve(spender, mininumAllowance.add(1).toBigNumber(), { gasLimit: 50000 });
+        .approve(spender, mininumAllowance.blockchainString, { gasLimit: 50000 });
       toast.confirming(approveTXN);
       const receipt = await approveTXN.wait();
       toast.success(receipt);
